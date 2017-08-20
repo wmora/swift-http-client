a2787f03b5ff7a773af4dab6118cc733de42a1f3
William Mora <william.r.mora@gmail.com>
2017-08-20 13:03:34 -0400

    Initial Commit

---
+++ HTTPClient/HTTPClient.h
@@ -0,0 +1,19 @@
+//
+//  HTTPClient.h
+//  HTTPClient
+//
+//  Created by William Mora on 2017-08-20.
+//  Copyright © 2017 William Mora. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HTTPClient.
+FOUNDATION_EXPORT double HTTPClientVersionNumber;
+
+//! Project version string for HTTPClient.
+FOUNDATION_EXPORT const unsigned char HTTPClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HTTPClient/PublicHeader.h>
+
+
