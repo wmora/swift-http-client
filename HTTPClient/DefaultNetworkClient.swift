92e085179624051431142810c8b272f0469e1926
William Mora <william.r.mora@gmail.com>
2017-08-27 15:04:08 -0400

    Replace ifs with guard statemtents

--- HTTPClient/DefaultNetworkClient.swift
+++ HTTPClient/DefaultNetworkClient.swift
@@ -11,21 +11,29 @@ import Foundation
 class DefaultNetworkClient: NetworkClient {
  
     func get(url: String, params: [String : Any], callback: @escaping (HTTPResponse) -> Void) {
-        let url = URL(string: "\(url)\(params.queryString())")!
-        let task = URLSession.shared.dataTask(with: url) { (data: Data?, response: URLResponse?, error: Error?) in
-            if let response = response as? HTTPURLResponse {
-                var responseData: [String: Any] = [:]
-                
-                if let data = data {
-                    do {
-                        responseData = try JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
-                    } catch let error {
-                        print(error)
-                    }
-                }
-                
-                callback(HTTPResponse(statusCode: response.statusCode, headers: response.allHeaderFields, data: responseData))
+        
+        guard let requestUrl = URL(string: "\(url)\(params.queryString())") else {
+            print("Invalid url \(url)")
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: requestUrl) { (data: Data?, response: URLResponse?, error: Error?) in
+            guard let response = response as? HTTPURLResponse else {
+                return
             }
+            
+            var responseData: [String: Any] = [:]
+            
+            if let data = data {
+                do {
+                    responseData = try JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                } catch let error {
+                    print(error)
+                }
+            }
+            
+            callback(HTTPResponse(statusCode: response.statusCode, headers: response.allHeaderFields, data: responseData))
+            
         }
         
         task.resume()
