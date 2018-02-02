34d167f486db1641828f8c14d7ad648ef42336fe
William Mora <william.r.mora@gmail.com>
2018-02-01 22:42:20 -0500

    Set up UI in Obj-C demo

--- DemoAppObjC/DemoAppObjC/ViewController.m
+++ DemoAppObjC/DemoAppObjC/ViewController.m
@@ -14,16 +14,20 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+-(void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(IBAction)getButtonDidTouchUpInside:(id)sender
+{
 }
 
+-(IBAction)putButtonDidTouchUpInside:(id)sender
+{
+}
+
+-(IBAction)postButtonDidTouchUpInside:(id)sender
+{
+}
 
 @end
